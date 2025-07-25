96d8345271d51e0f5b05bcadbfa6e0f0f31f0e5c
Simon Alexander <si.alexander@gmail.com>
2025-07-25 15:31:53 +0100

    Removed unneccessary script and tidied up return statements

--- SupportDeathClock/Private/Test-ConnectionToEOLDate.ps1
+++ SupportDeathClock/Private/Test-ConnectionToEOLDate.ps1
@@ -8,13 +8,13 @@ function Test-ConnectionToEOLDate {
         $response = Invoke-WebRequest -Uri $url -UseBasicParsing -ErrorAction Stop
         if ($response.StatusCode -eq 200) {
             Write-Verbose "Connection to endoflife.date API is successful."
-            return $true
+            $true
         } else {
             Write-Error "Failed to connect to endoflife.date API. Status code: $($response.StatusCode)"
-            return $false
+            $false
         }
     } catch {
         Write-Error "Error connecting to endoflife.date API: $_"
-        return $false
+        $false
     }
 }
